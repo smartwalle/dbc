e6d0003509cc03c22d647eb98c5ef4a355a59875
SmartWalle <smartwalle@gmail.com>
2021-06-27 10:43:46 +0800

    update test

--- cache_test.go
+++ cache_test.go
@@ -6,10 +6,27 @@ import (
 	"testing"
 )
 
-func BenchmarkCache_Set(b *testing.B) {
-	tc := dbc.New()
-	b.ResetTimer()
+func set(c dbc.Cache, b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		tc.Set("test"+strconv.Itoa(i), "value")
+		c.Set("sss"+strconv.Itoa(i), "hello")
 	}
+}
+
+func get(c dbc.Cache, b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		c.Get("sss" + strconv.Itoa(i))
+	}
+}
+
+func BenchmarkCache_Set(b *testing.B) {
+	c := dbc.New()
+	b.ResetTimer()
+	set(c, b)
+}
+
+func BenchmarkCache_Get(b *testing.B) {
+	c := dbc.New()
+	set(c, b)
+	b.ResetTimer()
+	get(c, b)
 }
